fa16c4cb968a0ab731d01756d0bb5278a17b706c
sam120900 <samindi1209@gmail.com>
2023-11-02 13:40:59 +1000

    loader implemented

--- app/public/script.js
+++ app/public/script.js
@@ -1,133 +1,155 @@
 async function getProductSentiment() {
-    const productUrl = document.getElementById('productUrl').value;
-    const response = await fetch(`/get-product-sentiment?productUrl=${encodeURIComponent(productUrl)}`);
+  const productUrl = document.getElementById("productUrl").value;
+  const resultDiv = document.getElementById("result");
+  const loader = document.getElementById("loader");
+
+  // Clear previous results and show loader
+  resultDiv.innerHTML = "";
+  loader.style.display = "block";
+
+  try {
+    const response = await fetch(
+      `/get-product-sentiment?productUrl=${encodeURIComponent(productUrl)}`
+    );
     const data = await response.json();
 
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
-
+    // Hide loader
+    loader.style.display = "none";
 
     if (data.error) {
-        // Display error message
-        resultDiv.innerHTML = `<div class="alert alert-danger">${data.error}</div>`;
-        return;
+      // Display error message
+      resultDiv.innerHTML = `<div class="alert alert-danger">${data.error}</div>`;
+      return;
     }
 
     // Display product info
-    const productInfoDiv = document.createElement('div');
-    productInfoDiv.className = 'product-info';
+    const productInfoDiv = document.createElement("div");
+    productInfoDiv.className = "product-info";
     productInfoDiv.innerHTML = `
-        <img src="${data.productImageUrl}" alt="${data.productTitle}" class="product-image">
-        <div>
-            <h3>${data.productTitle}</h3>
-            <div>${displayRatingStars(data.productRating)}</div>
-        </div>
-    `;
+            <img src="${data.productImageUrl}" alt="${
+      data.productTitle
+    }" class="product-image">
+            <div>
+                <h3>${data.productTitle}</h3>
+                <div>${displayRatingStars(data.productRating)}</div>
+            </div>
+        `;
     resultDiv.appendChild(productInfoDiv);
 
-
     if (data.message) {
-        // Display message if there are no reviews
-        const messageDiv = document.createElement('div');
-        messageDiv.style.textAlign = 'center'; // Center the text
-        messageDiv.style.marginTop = '20px'; // Add space above the message
-        messageDiv.innerHTML = `<div>${data.message}</div>`;
-        resultDiv.appendChild(messageDiv);
-        return;
+      // Display message if there are no reviews
+      const messageDiv = document.createElement("div");
+      messageDiv.style.textAlign = "center"; // Center the text
+      messageDiv.style.marginTop = "20px"; // Add space above the message
+      messageDiv.innerHTML = `<div>${data.message}</div>`;
+      resultDiv.appendChild(messageDiv);
+      return;
     }
 
     // Display sentiment score and emoji
-    const sentimentScoreDiv = document.createElement('div');
-    sentimentScoreDiv.className = 'sentiment-score';
+    const sentimentScoreDiv = document.createElement("div");
+    sentimentScoreDiv.className = "sentiment-score";
     sentimentScoreDiv.innerHTML = `
-        <span>Sentiment Score: ${data.averageScore.toFixed(2)}</span>
-        <span class="sentiment-emoji">
-            ${data.averageScore > 0 ? '😊' : (data.averageScore < 0 ? '😞' : '😐')}
-            </span>`;
+            <span>Sentiment Score: ${data.averageScore.toFixed(2)}</span>
+            <span class="sentiment-emoji">
+                ${
+                  data.averageScore > 0
+                    ? "😊"
+                    : data.averageScore < 0
+                    ? "😞"
+                    : "😐"
+                }
+                </span>`;
     resultDiv.appendChild(sentimentScoreDiv);
 
     // Display most common positive and negative words
-    const tableDiv = document.createElement('div');
+    const tableDiv = document.createElement("div");
     tableDiv.innerHTML = `
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Most Common Positive Words</th>
-                    <th>Most Common Negative Words</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>${data.mostCommonPositiveWords.join(', ')}</td>
-                    <td>${data.mostCommonNegativeWords.join(', ')}</td>
-                </tr>
-            </tbody>
-        </table>
-    `;
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Most Common Positive Words</th>
+                        <th>Most Common Negative Words</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>${data.mostCommonPositiveWords.join(", ")}</td>
+                        <td>${data.mostCommonNegativeWords.join(", ")}</td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
     resultDiv.appendChild(tableDiv);
 
     // Display product info
-    const getReviewsButtonDiv = document.createElement('div');
+    const getReviewsButtonDiv = document.createElement("div");
     getReviewsButtonDiv.innerHTML = `<button class="btn btn-primary" onclick="getReviewsFromS3()">Show 10 Random Reviews</button>`;
     resultDiv.appendChild(getReviewsButtonDiv);
+  } catch (error) {
+    console.error("Error fetching product sentiment:", error);
+    alert("Failed to fetch product sentiment.");
+    loader.style.display = "none"; // Hide loader in case of error
+  }
 }
 
 async function getReviewsFromS3() {
-    const s3ObjectKey = localStorage.getItem('s3Object');
-    if (!s3ObjectKey) {
-        alert('No reviews available.');
-        return;
+  const s3ObjectKey = localStorage.getItem("s3Object");
+  if (!s3ObjectKey) {
+    alert("No reviews available.");
+    return;
+  }
+
+  try {
+    // Fetch data from your server which will retrieve data from S3
+    const response = await fetch(
+      `/get-reviews-from-s3?key=${encodeURIComponent(s3ObjectKey)}`
+    );
+    const data = await response.json();
+
+    // Randomly select 10 reviews
+    const randomReviews = [];
+    const reviews = data.productReviews;
+    for (let i = 0; i < 10 && reviews.length > 0; i++) {
+      const randomIndex = Math.floor(Math.random() * reviews.length);
+      randomReviews.push(reviews.splice(randomIndex, 1)[0]);
     }
 
-    try {
-        // Fetch data from your server which will retrieve data from S3
-        const response = await fetch(`/get-reviews-from-s3?key=${encodeURIComponent(s3ObjectKey)}`);
-        const data = await response.json();
+    // Create a table to display the random reviews
+    const reviewsTable = document.createElement("table");
+    reviewsTable.className = "table table-striped";
+    const tableBody = document.createElement("tbody");
+    randomReviews.forEach((review, index) => {
+      const row = document.createElement("tr");
+      const cell = document.createElement("td");
+      cell.innerText = `${index + 1}. ${review}`;
+      row.appendChild(cell);
+      tableBody.appendChild(row);
+    });
+    reviewsTable.appendChild(tableBody);
 
-        // Randomly select 10 reviews
-        const randomReviews = [];
-        const reviews = data.productReviews;
-        for (let i = 0; i < 10 && reviews.length > 0; i++) {
-            const randomIndex = Math.floor(Math.random() * reviews.length);
-            randomReviews.push(reviews.splice(randomIndex, 1)[0]);
-        }
-
-        // Create a table to display the random reviews
-        const reviewsTable = document.createElement('table');
-        reviewsTable.className = 'table table-striped';
-        const tableBody = document.createElement('tbody');
-        randomReviews.forEach((review, index) => {
-            const row = document.createElement('tr');
-            const cell = document.createElement('td');
-            cell.innerText = `${index + 1}. ${review}`;
-            row.appendChild(cell);
-            tableBody.appendChild(row);
-        });
-        reviewsTable.appendChild(tableBody);
-
-        const resultDiv = document.getElementById('result');
-        resultDiv.appendChild(reviewsTable);
-    } catch (error) {
-        console.error('Error fetching reviews:', error);
-        alert('Failed to fetch reviews.');
-    }
+    const resultDiv = document.getElementById("result");
+    resultDiv.appendChild(reviewsTable);
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    alert("Failed to fetch reviews.");
+  }
 }
 
-
 function displayRatingStars(rating) {
-    const fullStars = Math.floor(rating);
-    const halfStar = rating % 1 >= 0.5 ? 1 : 0;
-    const emptyStars = 5 - fullStars - halfStar;
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 >= 0.5 ? 1 : 0;
+  const emptyStars = 5 - fullStars - halfStar;
 
-    let starsHtml = '';
-    for (let i = 0; i < fullStars; i++) {
-        starsHtml += '⭐';
-    }
-    if (halfStar) {
-        starsHtml += '⭐️';
-    }
-    for (let i = 0; i < emptyStars; i++) {
-        starsHtml += '⭐️';
-    }
-    return starsHtml;
+  let starsHtml = "";
+  for (let i = 0; i < fullStars; i++) {
+    starsHtml += "⭐";
+  }
+  if (halfStar) {
+    starsHtml += "⭐️";
+  }
+  for (let i = 0; i < emptyStars; i++) {
+    starsHtml += "⭐️";
+  }
+  return starsHtml;
 }
--- app/scrape.js
+++ app/scrape.js
@@ -1,16 +1,16 @@
-const puppeteer = require('puppeteer');
-const Sentiment = require('sentiment');
-const AWS = require('aws-sdk');
-const crypto = require('crypto');
+const puppeteer = require("puppeteer");
+const Sentiment = require("sentiment");
+const AWS = require("aws-sdk");
+const crypto = require("crypto");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Configure AWS SDK
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    sessionToken: process.env.AWS_SESSION_TOKEN,
-    region: "ap-southeast-2",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: process.env.AWS_SESSION_TOKEN,
+  region: "ap-southeast-2",
 });
 
 const s3 = new AWS.S3();
@@ -18,170 +18,185 @@ const s3 = new AWS.S3();
 const sentiment = new Sentiment();
 
 async function scrapeAmazonProductReviews(productUrl) {
-    // Check if URL is provided
-    if (!productUrl) {
-        return "URL not provided!";
-    }
+  // Check if URL is provided
+  if (!productUrl) {
+    return "URL not provided!";
+  }
 
-    // Check if the URL is valid
-    try {
-        new URL(productUrl);
-    } catch (error) {
-        return "Invalid URL!";
-    }
+  // Check if the URL is valid
+  try {
+    new URL(productUrl);
+  } catch (error) {
+    return "Invalid URL!";
+  }
 
-    // Check if the URL contains amazon.com
-    if (!productUrl.includes('amazon.com')) {
-        return "Sorry, we only accept links to products on Amazon";
-    }
+  // Check if the URL contains amazon.com
+  if (!productUrl.includes("amazon.com")) {
+    return "Sorry, we only accept links to products on Amazon";
+  }
 
+  // Trim the URL to get the base URL of the product
+  productUrlParsed = `"${productUrl}"`;
 
-    // Trim the URL to get the base URL of the product
-    productUrlParsed = `"${productUrl}"`
+  const shortUrl = productUrl.split("/ref")[0] + "/";
 
-    const shortUrl = productUrl.split('/ref')[0] + '/';
+  const reviewUrl =
+    shortUrl.replace("/dp/", "/product-reviews/") +
+    "ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews";
 
-    const reviewUrl = shortUrl.replace('/dp/', '/product-reviews/') + 'ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews';
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  // Go to the product page to extract product details
+  await page.goto(shortUrl);
 
-    // Go to the product page to extract product details
-    await page.goto(shortUrl);
+  // Extract product title, image URL, and rating
+  const productDetails = await page.evaluate(() => {
+    const title = document.querySelector("#productTitle")
+      ? document.querySelector("#productTitle").innerText.trim()
+      : null;
+    const imageUrl = document.querySelector("#imgTagWrapperId img")
+      ? document.querySelector("#imgTagWrapperId img").src
+      : null;
+    const ratingElement = document.querySelector(".a-size-base.a-color-base"); // Updated selector
+    const rating = ratingElement ? ratingElement.innerText.trim() : null;
+    return { title, imageUrl, rating };
+  });
 
-    // Extract product title, image URL, and rating
-    const productDetails = await page.evaluate(() => {
-        const title = document.querySelector('#productTitle') ? document.querySelector('#productTitle').innerText.trim() : null;
-        const imageUrl = document.querySelector('#imgTagWrapperId img') ? document.querySelector('#imgTagWrapperId img').src : null;
-        const ratingElement = document.querySelector('.a-size-base.a-color-base'); // Updated selector
-        const rating = ratingElement ? ratingElement.innerText.trim() : null;
-        return { title, imageUrl, rating };
+  // Go to the reviews page
+  await page.goto(reviewUrl, { waitUntil: "networkidle0" });
+
+  let allReviews = [];
+
+  while (true) {
+    // Extract reviews from the current page
+    const reviews = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll(".review-text-content")).map(
+        (review) => review.innerText.trim()
+      );
     });
 
+    allReviews = allReviews.concat(reviews);
 
-    // Go to the reviews page
-    await page.goto(reviewUrl, { waitUntil: 'networkidle0' });
-
-    let allReviews = [];
-
-    while (true) {
-        // Extract reviews from the current page
-        const reviews = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('.review-text-content')).map(review => review.innerText.trim());
-        });
-
-        allReviews = allReviews.concat(reviews);
-
-        // Try to find the 'Next Page' URL
-        const nextPageUrl = await page.evaluate(() => {
-            const nextPageLink = document.querySelector('li.a-last a');
-            return nextPageLink ? nextPageLink.href : null;
-        });
-
-        if (nextPageUrl) {
-            await page.goto(nextPageUrl, { waitUntil: 'networkidle0' });
-        } else {
-            // No more pages
-            break;
-        }
-    }
-
-    await browser.close();
-
-    // Check if there are no reviews
-    if (allReviews.length === 0) {
-        return {
-            productUrl: productUrl,
-            productTitle: productDetails.title,
-            productImageUrl: productDetails.imageUrl,
-            productRating: productDetails.rating,
-            message: "This product has no reviews yet!"
-        };
-    }
-    else {
-        const hash = crypto.createHash('sha256').update(productDetails.title).digest('hex');
-        const fileName = `${hash}.json`;
-
-        const dataToSave = {
-            productUrl: url,
-            productTitle: productDetails.title,
-            productImageUrl: productDetails.imageUrl,
-            productRating: productDetails.rating,
-            averageScore,
-            averageComparative,
-            mostCommonPositiveWords: sortedPositiveWords,
-            mostCommonNegativeWords: sortedNegativeWords,
-            numberOfReviewsAnalysed: allReviews.length,
-            productReviews: allReviews
-        };
-
-        // Upload to S3
-        const params = {
-            Bucket: 'S3_BUCKET_NAME',
-            Key: fileName,
-            Body: JSON.stringify(dataToSave),
-            ContentType: 'application/json'
-        };
-
-        s3.upload(params, function (err, data) {
-            if (err) {
-                console.error("Error uploading to S3:", err);
-            } else {
-                console.log("Successfully uploaded data to S3:", data.Location);
-                // Store the S3 object key in local storage
-                localStorage.setItem('s3Object', fileName);
-            }
-        });
-    }
-
-    // Perform sentiment analysis on each review
-    const sentimentResults = allReviews.map(review => {
-        return {
-            review,
-            sentiment: sentiment.analyze(review)
-        };
+    // Try to find the 'Next Page' URL
+    const nextPageUrl = await page.evaluate(() => {
+      const nextPageLink = document.querySelector("li.a-last a");
+      return nextPageLink ? nextPageLink.href : null;
     });
 
-    // Initialize variables for averages and word frequencies
-    let totalScore = 0;
-    let totalComparative = 0;
-    const positiveWordFrequency = {};
-    const negativeWordFrequency = {};
+    if (nextPageUrl) {
+      await page.goto(nextPageUrl, { waitUntil: "networkidle0" });
+    } else {
+      // No more pages
+      break;
+    }
+  }
 
-    // Loop through sentiment results to calculate averages and word frequencies
-    sentimentResults.forEach(result => {
-        totalScore += result.sentiment.score;
-        totalComparative += result.sentiment.comparative;
+  await browser.close();
 
-        result.sentiment.positive.forEach(word => {
-            positiveWordFrequency[word] = (positiveWordFrequency[word] || 0) + 1;
-        });
-
-        result.sentiment.negative.forEach(word => {
-            negativeWordFrequency[word] = (negativeWordFrequency[word] || 0) + 1;
-        });
-    });
-
-    // Calculate averages
-    const averageScore = totalScore / sentimentResults.length;
-    const averageComparative = totalComparative / sentimentResults.length;
-
-    // Sort and get top 5 positive and negative words
-    const sortedPositiveWords = Object.entries(positiveWordFrequency).sort((a, b) => b[1] - a[1]).slice(0, 5).map(item => item[0]);
-    const sortedNegativeWords = Object.entries(negativeWordFrequency).sort((a, b) => b[1] - a[1]).slice(0, 5).map(item => item[0]);
-
-    // Return the overall object
+  // Check if there are no reviews
+  if (allReviews.length === 0) {
     return {
-        productUrl: productUrl,
-        productTitle: productDetails.title,
-        productImageUrl: productDetails.imageUrl,
-        productRating: productDetails.rating,
-        averageScore,
-        averageComparative,
-        mostCommonPositiveWords: sortedPositiveWords,
-        mostCommonNegativeWords: sortedNegativeWords,
-        numberOfReviewsAnalysed: allReviews.length
+      productUrl: productUrl,
+      productTitle: productDetails.title,
+      productImageUrl: productDetails.imageUrl,
+      productRating: productDetails.rating,
+      message: "This product has no reviews yet!",
     };
+  }
+
+  // Perform sentiment analysis on each review
+  const sentimentResults = allReviews.map((review) => {
+    return {
+      review,
+      sentiment: sentiment.analyze(review),
+    };
+  });
+
+  // Initialize variables for averages and word frequencies
+  let totalScore = 0;
+  let totalComparative = 0;
+  const positiveWordFrequency = {};
+  const negativeWordFrequency = {};
+
+  // Loop through sentiment results to calculate averages and word frequencies
+  sentimentResults.forEach((result) => {
+    totalScore += result.sentiment.score;
+    totalComparative += result.sentiment.comparative;
+
+    result.sentiment.positive.forEach((word) => {
+      positiveWordFrequency[word] = (positiveWordFrequency[word] || 0) + 1;
+    });
+
+    result.sentiment.negative.forEach((word) => {
+      negativeWordFrequency[word] = (negativeWordFrequency[word] || 0) + 1;
+    });
+  });
+
+  // Calculate averages
+  const averageScore = totalScore / sentimentResults.length;
+  const averageComparative = totalComparative / sentimentResults.length;
+
+  // Sort and get top 5 positive and negative words
+  const sortedPositiveWords = Object.entries(positiveWordFrequency)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 5)
+    .map((item) => item[0]);
+  const sortedNegativeWords = Object.entries(negativeWordFrequency)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 5)
+    .map((item) => item[0]);
+
+  if (allReviews.length > 0) {
+    const hash = crypto
+      .createHash("sha256")
+      .update(productDetails.title)
+      .digest("hex");
+    const fileName = `${hash}.json`;
+
+    const dataToSave = {
+      productUrl: productUrl,
+      productTitle: productDetails.title,
+      productImageUrl: productDetails.imageUrl,
+      productRating: productDetails.rating,
+      averageScore,
+      averageComparative,
+      mostCommonPositiveWords: sortedPositiveWords,
+      mostCommonNegativeWords: sortedNegativeWords,
+      numberOfReviewsAnalysed: allReviews.length,
+      productReviews: allReviews,
+    };
+
+    // Upload to S3
+    const params = {
+      Bucket: "S3_BUCKET_NAME",
+      Key: fileName,
+      Body: JSON.stringify(dataToSave),
+      ContentType: "application/json",
+    };
+
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.error("Error uploading to S3:", err);
+      } else {
+        console.log("Successfully uploaded data to S3:", data.Location);
+        // Store the S3 object key in local storage
+        localStorage.setItem("s3Object", fileName);
+      }
+    });
+  }
+  // Return the overall object
+  return {
+    productUrl: productUrl,
+    productTitle: productDetails.title,
+    productImageUrl: productDetails.imageUrl,
+    productRating: productDetails.rating,
+    averageScore,
+    averageComparative,
+    mostCommonPositiveWords: sortedPositiveWords,
+    mostCommonNegativeWords: sortedNegativeWords,
+    numberOfReviewsAnalysed: allReviews.length,
+  };
 }
 
 // Test function
