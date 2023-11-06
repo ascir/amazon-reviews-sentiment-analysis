c005608daedb0c2e75d76cad6385f4e5e8580039
Anjana Ranjan <anjanar876@gmail.com>
2023-11-06 10:27:48 +1000

    redis integration

--- app/app.js
+++ app/app.js
@@ -4,10 +4,30 @@ const cors = require('cors');
 const responseTime = require('response-time')
 const redis = require('redis');
 
-const { scrapeAmazonProductReviews, getReviewsFromS3 }  = require('./backendFunctions');
+const { scrapeAmazonProductReviews, getReviewsFromS3 } = require('./backendFunctions');
 
 const app = express();
 const port = 3000;
+
+// Create and connect to the Redis client
+const redisClient = redis.createClient({
+    url: 'redis://redis:6379' // This uses the URL format to specify the Redis server location
+  });
+  
+  // Connect to Redis
+  (async () => {
+    try {
+      await redisClient.connect();
+      console.log('Connected to Redis');
+    } catch (err) {
+      console.error('Redis connection error', err);
+    }
+  })();
+
+  // Error event listener
+redisClient.on('error', (err) => {
+    console.error('Redis error', err);
+  });
 
 app.use(logger('tiny'));
 require('dotenv').config();
@@ -18,18 +38,6 @@ app.use(cors()); // Prevents CORS error
 // Render static page
 app.use(express.static('public'));
 
-
-// Create redis client
-const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-  });
-  
-  // Connect to Redis
-  redisClient.connect()
-  .catch((err) => {
-    console.log(err);
-  })
 
 // Endpoint to handle Amazon URL requests
 app.get('/get-product-sentiment', async (req, res) => {
@@ -43,15 +51,29 @@ app.get('/get-product-sentiment', async (req, res) => {
         return res.status(400).json({ error: 'Sorry we only accept links to products on Amazon' });
     }
 
-    try {
-        const sentimentResults = await scrapeAmazonProductReviews(productUrl);
-        console.log(sentimentResults);
+    // Use the product URL as the key for Redis
+    const cacheKey = `sentiment:${productUrl}`;
 
-        if (typeof sentimentResults === 'string') {
-            // If the result is a string, it's an error message
-            res.status(400).send(sentimentResults);
+    try {
+        // Check if the data is in Redis
+        const cachedData = await redisClient.get(cacheKey);
+
+        if (cachedData) {
+            // If data is found in Redis cache, parse it and return
+            return res.json(JSON.parse(cachedData));
         } else {
-            res.send(sentimentResults);
+            // If not found, scrape the data
+            const sentimentResults = await scrapeAmazonProductReviews(productUrl);
+            console.log(sentimentResults);
+
+            if (typeof sentimentResults === 'string') {
+                // If the result is a string, it's an error message
+                res.status(400).send(sentimentResults);
+            } else {
+                // Save the scraped data in Redis with an expiration time of 1 hour
+                await redisClient.setEx(cacheKey, 3600, JSON.stringify(sentimentResults));
+                res.send(sentimentResults);
+            }
         }
     } catch (error) {
         res.status(500).send({ error: error.message });
@@ -60,7 +82,7 @@ app.get('/get-product-sentiment', async (req, res) => {
 
 
 app.get('/get-reviews-from-s3', async (req, res) => {
-    const key = req.query.key; 
+    const key = req.query.key;
 
     if (!key) {
         return res.status(400).json({ error: 'S3 key is required' });
